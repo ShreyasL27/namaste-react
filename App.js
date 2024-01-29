282305a41f2a756864e6ccb52221382a802cce33
ShreyasL27 <shreyaslinge1@gmail.com>
2024-01-29 15:37:40 +0530

    Learning Namaste React - ep 01

---
+++ App.js
@@ -0,0 +1,23 @@
+/*
+<div id="parent">
+    <div id="child">
+        <h1>Hello World!</h1>
+        <p>This is a paragraph</p>
+    </div>
+</div>
+*/
+
+const elements = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading" }, "Hello World!"),
+    React.createElement("p", { id: "para" }, "This is a paragraph"),
+  ])
+);
+
+// const heading = React.createElement("h1", {id : "heading"}, "Hello World! by React using CDN");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(elements);
---
+++ README.md
@@ -0,0 +1,1 @@
+# Namaste learning React 🚀
